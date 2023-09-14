b3d57e409b828fdc54258b44782f405e6a714ff4
Vishal B <63901924+vishallb@users.noreply.github.com>
2023-09-14 18:38:57 +0530

    Create 182. Duplicate Emails.sql

---
+++ Easy/182. Duplicate Emails.sql
@@ -0,0 +1,21 @@
+/* Table: Person
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| id          | int     |
+| email       | varchar |
++-------------+---------+
+id is the primary key (column with unique values) for this table.
+Each row of this table contains an email. The emails will not contain uppercase letters.
+
+Write a solution to report all the duplicate emails. Note that it's guaranteed that the email field is not NULL.
+Return the result table in any order.*/
+
+# solution
+SELECT
+  email as Email
+FROM
+  Person
+GROUP BY email
+HAVING COUNT(*) > 1;
