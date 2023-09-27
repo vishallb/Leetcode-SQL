87b2913d1aafc3b0aaee32c423c6abd1b88b7d1f
Vishal B <63901924+vishallb@users.noreply.github.com>
2023-09-27 17:10:31 +0530

    Update 627. Swap Salary.sql

--- Easy/627. Swap Salary.sql
+++ Easy/627. Swap Salary.sql
@@ -16,7 +16,7 @@ The table contains information about an employee.
 Write a solution to swap all 'f' and 'm' values (i.e., change all 'f' values to 'm' and vice versa) with a single update statement and no intermediate temporary tables.*/
 
 # solution
-PDATE SALARY
+UPDATE SALARY
 SET sex = CASE
             WHEN sex='f' THEN 'm' 
             WHEN sex='m' THEN 'f'
