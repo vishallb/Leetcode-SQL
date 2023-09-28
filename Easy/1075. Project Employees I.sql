c5b11c6eca7ac1c8aef8eb191d6d9d567c158cd6
Vishal B <63901924+vishallb@users.noreply.github.com>
2023-09-28 20:43:53 +0530

    Create 1075. Project Employees I.sql

---
+++ Easy/1075. Project Employees I.sql
@@ -0,0 +1,38 @@
+/*Table: Project
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| project_id  | int     |
+| employee_id | int     |
++-------------+---------+
+(project_id, employee_id) is the primary key of this table.
+employee_id is a foreign key to Employee table.
+Each row of this table indicates that the employee with employee_id is working on the project with project_id.
+ 
+
+Table: Employee
+
++------------------+---------+
+| Column Name      | Type    |
++------------------+---------+
+| employee_id      | int     |
+| name             | varchar |
+| experience_years | int     |
++------------------+---------+
+employee_id is the primary key of this table. It's guaranteed that experience_years is not NULL.
+Each row of this table contains information about one employee.
+ 
+
+Write an SQL query that reports the average experience years of all the employees for each project, rounded to 2 digits.*/
+
+# solution
+
+SELECT
+    project_id,
+    ROUND(AVG(experience_years), 2) AS average_years
+FROM
+    Project p
+INNER JOIN Employee e
+ON p.employee_id = e.employee_id
+GROUP BY project_id;
