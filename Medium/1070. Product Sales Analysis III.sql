bfa826ebc2cb5236445b9f324c2e56f0dca19100
Vishal B <63901924+vishallb@users.noreply.github.com>
2023-10-05 09:50:00 +0530

    Create 1070. Product Sales Analysis III.sql

---
+++ Medium/1070. Product Sales Analysis III.sql
@@ -0,0 +1,53 @@
+/*Table: Sales
+
++-------------+-------+
+| Column Name | Type  |
++-------------+-------+
+| sale_id     | int   |
+| product_id  | int   |
+| year        | int   |
+| quantity    | int   |
+| price       | int   |
++-------------+-------+
+(sale_id, year) is the primary key (combination of columns with unique values) of this table.
+product_id is a foreign key (reference column) to Product table.
+Each row of this table shows a sale on the product product_id in a certain year.
+Note that the price is per unit.
+ 
+
+Table: Product
+
++--------------+---------+
+| Column Name  | Type    |
++--------------+---------+
+| product_id   | int     |
+| product_name | varchar |
++--------------+---------+
+product_id is the primary key (column with unique values) of this table.
+Each row of this table indicates the product name of each product.
+ 
+
+Write a solution to select the product id, year, quantity, and price for the first year of every product sold.
+
+Return the resulting table in any order.*/
+
+# solution
+WITH cte as (
+    SELECT
+        product_id,
+        year,
+        quantity,
+        price,
+        RANK() OVER(PARTITION BY product_id ORDER BY year ASC) as rnk
+    FROM
+        Sales
+)
+
+SELECT
+    product_id,
+    year as first_year,
+    quantity,
+    price
+FROM
+    cte
+WHERE rnk = 1;
