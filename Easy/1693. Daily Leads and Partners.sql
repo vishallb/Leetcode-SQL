061f8515d7455c4b912d2697fc8a520e41f75188
Vishal B <63901924+vishallb@users.noreply.github.com>
2023-10-14 13:00:16 +0530

    Create 1693. Daily Leads and Partners.sql

---
+++ Easy/1693. Daily Leads and Partners.sql
@@ -0,0 +1,25 @@
+/*Table: DailySales
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| date_id     | date    |
+| make_name   | varchar |
+| lead_id     | int     |
+| partner_id  | int     |
++-------------+---------+
+There is no primary key (column with unique values) for this table. It may contain duplicates.
+This table contains the date and the name of the product sold and the IDs of the lead and partner it was sold to.
+The name consists of only lowercase English letters.
+ 
+For each date_id and make_name, find the number of distinct lead_id's and distinct partner_id's.*/
+
+-- solution
+SELECT
+    date_id,
+    make_name,
+    COUNT(DISTINCT lead_id) as unique_leads,
+    COUNT(DISTINCT partner_id) as unique_partners
+FROM
+    DailySales
+GROUP BY 1, 2;
