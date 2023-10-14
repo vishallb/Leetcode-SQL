027fda4c14d1ddf4ba2d4aadf80ecb3af44e975f
Vishal B <63901924+vishallb@users.noreply.github.com>
2023-10-14 13:04:23 +0530

    Create 2356. Number of Unique Subjects Taught by Each Teacher.sql

---
+++ Easy/2356. Number of Unique Subjects Taught by Each Teacher.sql
@@ -0,0 +1,22 @@
+/*Table: Teacher
+
++-------------+------+
+| Column Name | Type |
++-------------+------+
+| teacher_id  | int  |
+| subject_id  | int  |
+| dept_id     | int  |
++-------------+------+
+(subject_id, dept_id) is the primary key (combinations of columns with unique values) of this table.
+Each row in this table indicates that the teacher with teacher_id teaches the subject subject_id in the department dept_id.
+ 
+
+Write a solution to calculate the number of unique subjects each teacher teaches in the university.*/
+
+-- solution
+SELECT
+    teacher_id,
+    COUNT(DISTINCT subject_id) as cnt
+FROM
+    Teacher
+GROUP BY 1;
