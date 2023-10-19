f49dcf14e150b79fc88de4a0e85ca76928b8f41f
Vishal B <63901924+vishallb@users.noreply.github.com>
2023-10-19 21:37:53 +0530

    Create 1407. Top Travellers.sql

---
+++ Easy/1407. Top Travellers.sql
@@ -0,0 +1,39 @@
+/*Table: Users
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| id            | int     |
+| name          | varchar |
++---------------+---------+
+id is the column with unique values for this table.
+name is the name of the user.
+ 
+
+Table: Rides
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| id            | int     |
+| user_id       | int     |
+| distance      | int     |
++---------------+---------+
+id is the column with unique values for this table.
+user_id is the id of the user who traveled the distance "distance".
+ 
+
+Write a solution to report the distance traveled by each user.
+
+Return the result table ordered by travelled_distance in descending order, if two or more users traveled the same distance, order them by their name in ascending order.*/
+
+# solution
+SELECT
+    u.name,
+    IFNULL(SUM(distance), 0) AS travelled_distance
+FROM
+    Users u
+LEFT JOIN Rides r
+ON u.id = r.user_id
+GROUP BY u.id
+ORDER BY 2 DESC, 1 ASC;
