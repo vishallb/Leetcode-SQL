c42df10947cd9e48732b891da34ffc6c53347386
Vishal B <63901924+vishallb@users.noreply.github.com>
2023-10-30 20:12:56 +0530

    Create 1527. Patients With a Condition.sql

---
+++ Easy/1527. Patients With a Condition.sql
@@ -0,0 +1,24 @@
+/*Table: Patients
+
++--------------+---------+
+| Column Name  | Type    |
++--------------+---------+
+| patient_id   | int     |
+| patient_name | varchar |
+| conditions   | varchar |
++--------------+---------+
+patient_id is the primary key (column with unique values) for this table.
+'conditions' contains 0 or more code separated by spaces. 
+This table contains information of the patients in the hospital.
+ 
+
+Write a solution to find the patient_id, patient_name, and conditions of the patients who have Type I Diabetes. Type I Diabetes always starts with DIAB1 prefix.*/
+
+--- solution
+SELECT 
+    patient_id,
+    patient_name,
+    conditions 
+FROM 
+    patients 
+WHERE conditions like "% DIAB1%" OR conditions like "DIAB1%";
