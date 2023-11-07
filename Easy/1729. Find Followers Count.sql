494bfc3ff4adfa06d732293b4d38ef61bb5c4a67
Vishal B <63901924+vishallb@users.noreply.github.com>
2023-11-07 22:33:40 +0530

    Create 1729. Find Followers Count.sql

---
+++ Easy/1729. Find Followers Count.sql
@@ -0,0 +1,24 @@
+/*Table: Followers
+
++-------------+------+
+| Column Name | Type |
++-------------+------+
+| user_id     | int  |
+| follower_id | int  |
++-------------+------+
+(user_id, follower_id) is the primary key (combination of columns with unique values) for this table.
+This table contains the IDs of a user and a follower in a social media app where the follower follows the user.
+ 
+
+Write a solution that will, for each user, return the number of followers.
+
+Return the result table ordered by user_id in ascending order.*/
+
+-- solution
+SELECT
+    user_id,
+    COUNT(follower_id) as followers_count
+FROM
+    Followers
+GROUP BY 1
+ORDER BY 1;
