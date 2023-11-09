dab0373f4cb453c0fd81e739019e7bc295a48c35
Vishal B <63901924+vishallb@users.noreply.github.com>
2023-11-09 22:07:52 +0530

    Create 1890. The Latest Login in 2020.sql

---
+++ Easy/1890. The Latest Login in 2020.sql
@@ -0,0 +1,21 @@
+/*Table: Logins
+
++----------------+----------+
+| Column Name    | Type     |
++----------------+----------+
+| user_id        | int      |
+| time_stamp     | datetime |
++----------------+----------+
+(user_id, time_stamp) is the primary key (combination of columns with unique values) for this table.
+Each row contains information about the login time for the user with ID user_id.
+ 
+Write a solution to report the latest login for all users in the year 2020. Do not include the users who did not login in 2020.*/
+
+-- solution
+SELECT
+    user_id,
+    MAX(time_stamp) as last_stamp
+FROM
+    Logins
+WHERE EXTRACT(YEAR FROM time_stamp) = 2020
+group by 1;
