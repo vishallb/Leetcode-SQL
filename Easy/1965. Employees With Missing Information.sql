92af0ed659a9d30e5fdf2a2e4567818154267d2d
Vishal B <63901924+vishallb@users.noreply.github.com>
2023-11-12 22:11:30 +0530

    Create 1965. Employees With Missing Information.sql

---
+++ Easy/1965. Employees With Missing Information.sql
@@ -0,0 +1,42 @@
+/*Table: Employees
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| employee_id | int     |
+| name        | varchar |
++-------------+---------+
+employee_id is the column with unique values for this table.
+Each row of this table indicates the name of the employee whose ID is employee_id.
+ 
+
+Table: Salaries
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| employee_id | int     |
+| salary      | int     |
++-------------+---------+
+employee_id is the column with unique values for this table.
+Each row of this table indicates the salary of the employee whose ID is employee_id.
+ 
+
+Write a solution to report the IDs of all the employees with missing information. The information of an employee is missing if:
+
+The employee's name is missing, or
+The employee's salary is missing.
+Return the result table ordered by employee_id in ascending order.*/
+
+--- solution
+SELECT employee_id
+FROM employees
+WHERE employee_id NOT IN (SELECT employee_id FROM salaries)
+
+UNION
+
+SELECT employee_id
+FROM salaries
+WHERE employee_id NOT IN (SELECT employee_id FROM employees)
+
+ORDER BY employee_id;
